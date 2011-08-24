85035e684f5c35d7065c7c03efa8c1ca13ee5ee2
croesch <christianroesch@gmx.net>
2011-08-24 19:16:44 +0200

    removed javadoc warning

--- src/main/java/com/github/croesch/listener/ErrorListener.java
+++ src/main/java/com/github/croesch/listener/ErrorListener.java
@@ -4,7 +4,7 @@ package com.github.croesch.listener;
  * Listener, that listens to error changes
  * 
  * @author croesch
- * @since (Date: 2011/01/13 21:12:39)
+ * @since Date: 2011/01/13
  */
 public abstract class ErrorListener {
 
@@ -12,8 +12,7 @@ public abstract class ErrorListener {
    * Will be invoked when the state of error changes
    * 
    * @param error {@code true}, if there is an error now
-   * @author croesch
-   * @since (Date: 13.01.2011 22:08:17)
+   * @since Date: 13.01.2011
    */
   public abstract void errorStateChanged(boolean error);
 }
