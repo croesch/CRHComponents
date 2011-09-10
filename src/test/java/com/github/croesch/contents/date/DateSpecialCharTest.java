104cd7f5a8afd09ca1b91a6ef032e09f80584ad1
croesch <christianroesch@gmx.net>
2011-09-11 00:04:45 +0200

    added license information to each file

--- src/test/java/com/github/croesch/contents/date/DateSpecialCharTest.java
+++ src/test/java/com/github/croesch/contents/date/DateSpecialCharTest.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2011  Christian Roesch
+ * 
+ * This file is part of crhcomponents.
+ * 
+ * crhcomponents is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * crhcomponents is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with crhcomponents.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.github.croesch.contents.date;
 
 import static com.github.croesch.contents.date.DateSpecialChar.ValueType.CONSTANT;
--- src/test/java/com/github/croesch/util/UtilTest.java
+++ src/test/java/com/github/croesch/util/UtilTest.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2011  Christian Roesch
+ * 
+ * This file is part of crhcomponents.
+ * 
+ * crhcomponents is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * crhcomponents is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with crhcomponents.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.github.croesch.util;
 
 import static org.fest.assertions.Assertions.assertThat;
@@ -22,7 +40,6 @@ public class UtilTest extends FestSwingJUnitTestCase {
    * @since Date: 17.02.2011 20:50:42
    */
   @Test
-  @SuppressWarnings("nls")
   public void testMultiply() {
     assertThat(Util.of("")).isEqualTo(new StringUtil(""));
     assertThat(Util.of("")).isNotEqualTo(new StringUtil("a"));
