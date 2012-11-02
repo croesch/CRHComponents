a9477dc6c3859090891266d3a807e791cf830059
croesch <christianroesch@gmx.net>
2012-11-03 00:06:36 +0100

    #17 implemented combobox

---
+++ src/main/java/com/github/croesch/components/CComboBox.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2011-2012  Christian Roesch
+ * 
+ * This file is part of crhcomponents.
+ * 
+ * crhcomponents is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * crhcomponents is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with crhcomponents.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.croesch.components;
+
+import java.util.Vector;
+
+import javax.swing.ComboBoxModel;
+import javax.swing.JComboBox;
+
+/**
+ * An extension of {@link JComboBox} that contains some default behavior that not each client should have to implement.
+ * 
+ * @author croesch
+ * @since Date: Nov 2, 2012
+ */
+public class CComboBox extends JComboBox {
+
+  /** generated serial version UID */
+  private static final long serialVersionUID = 6060299403463833180L;
+
+  /**
+   * Constructs a new combobox with the given name.
+   * 
+   * @since Date: Nov 2, 2012
+   * @param name the name of the component
+   */
+  public CComboBox(final String name) {
+    setName(name);
+  }
+
+  /**
+   * Constructs a new combobox with the given name and the given model.
+   * 
+   * @since Date: Nov 2, 2012
+   * @param name the name of the component
+   * @param aModel the model providing the list of items for the combobox
+   */
+  public CComboBox(final String name, final ComboBoxModel aModel) {
+    super(aModel);
+    setName(name);
+  }
+
+  /**
+   * Constructs a new combobox with the given name and the given objects for the list.
+   * 
+   * @since Date: Nov 2, 2012
+   * @param name the name of the component
+   * @param items the items for the combobox
+   */
+  public CComboBox(final String name, final Object[] items) {
+    super(items);
+    setName(name);
+  }
+
+  /**
+   * Constructs a new combobox with the given name and the given objects for the list.
+   * 
+   * @since Date: Nov 2, 2012
+   * @param name the name of the component
+   * @param items the items for the combobox
+   */
+  public CComboBox(final String name, final Vector<?> items) {
+    super(items);
+    setName(name);
+  }
+}
---
+++ src/test/java/com/github/croesch/components/CComboBoxTest.java
@@ -0,0 +1,167 @@
+/*
+ * Copyright (C) 2011-2012  Christian Roesch
+ * 
+ * This file is part of crhcomponents.
+ * 
+ * crhcomponents is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * crhcomponents is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with crhcomponents.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.croesch.components;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.Vector;
+
+import javax.swing.ComboBoxModel;
+import javax.swing.DefaultComboBoxModel;
+
+import org.fest.swing.edt.GuiActionRunner;
+import org.fest.swing.edt.GuiQuery;
+import org.fest.swing.fixture.JComboBoxFixture;
+import org.junit.Test;
+
+import com.github.croesch.DefaultTestCase;
+
+/**
+ * Provides test cases for {@link CComboBox}.
+ * 
+ * @author croesch
+ * @since Date: Nov 2, 2012
+ */
+public class CComboBoxTest extends DefaultTestCase {
+
+  public static CComboBox getComboBox(final String name) {
+    return GuiActionRunner.execute(new GuiQuery<CComboBox>() {
+      @Override
+      protected CComboBox executeInEDT() throws Throwable {
+        return new CComboBox(name);
+      }
+    });
+  }
+
+  public static CComboBox getComboBox(final String name, final ComboBoxModel model) {
+    return GuiActionRunner.execute(new GuiQuery<CComboBox>() {
+      @Override
+      protected CComboBox executeInEDT() throws Throwable {
+        return new CComboBox(name, model);
+      }
+    });
+  }
+
+  public static CComboBox getComboBox(final String name, final Object[] items) {
+    return GuiActionRunner.execute(new GuiQuery<CComboBox>() {
+      @Override
+      protected CComboBox executeInEDT() throws Throwable {
+        return new CComboBox(name, items);
+      }
+    });
+  }
+
+  public static CComboBox getComboBox(final String name, final Vector<?> items) {
+    return GuiActionRunner.execute(new GuiQuery<CComboBox>() {
+      @Override
+      protected CComboBox executeInEDT() throws Throwable {
+        return new CComboBox(name, items);
+      }
+    });
+  }
+
+  @Test
+  public void testCstr_String() {
+    JComboBoxFixture cb = new JComboBoxFixture(robot(), getComboBox("cb"));
+    assertThat(cb.component().getName()).isEqualTo("cb");
+
+    cb = new JComboBoxFixture(robot(), getComboBox(""));
+    assertThat(cb.component().getName()).isEqualTo("");
+
+    cb = new JComboBoxFixture(robot(), getComboBox(null));
+    assertThat(cb.component().getName()).isNull();
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void testCstr_NPXNullModel() {
+    getComboBox("cb", (ComboBoxModel) null);
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void testCstr_NPXNullObjectArray() {
+    getComboBox("cb", (Object[]) null);
+  }
+
+  @Test
+  public void testCstr_StringComboBoxModel() {
+    JComboBoxFixture cb = new JComboBoxFixture(robot(), getComboBox("cb", new DefaultComboBoxModel()));
+    assertThat(cb.component().getName()).isEqualTo("cb");
+    cb.requireItemCount(0);
+
+    cb = new JComboBoxFixture(robot(), getComboBox("", new DefaultComboBoxModel(new Object[12])));
+    assertThat(cb.component().getName()).isEqualTo("");
+    cb.requireItemCount(12);
+    for (int i = 0; i < cb.component().getModel().getSize(); ++i) {
+      assertThat(cb.valueAt(i)).isEqualTo("");
+    }
+
+    cb = new JComboBoxFixture(robot(),
+                              getComboBox(null, new DefaultComboBoxModel(new Object[] { "Peter", 15, "Hans" })));
+    assertThat(cb.component().getName()).isNull();
+    cb.requireItemCount(3);
+    assertThat(cb.valueAt(0)).isEqualTo("Peter");
+    assertThat(cb.valueAt(1)).isEqualTo("15");
+    assertThat(cb.valueAt(2)).isEqualTo("Hans");
+  }
+
+  @Test
+  public void testCstr_StringObject() {
+    JComboBoxFixture cb = new JComboBoxFixture(robot(), getComboBox("cb", new Object[0]));
+    assertThat(cb.component().getName()).isEqualTo("cb");
+    cb.requireItemCount(0);
+
+    cb = new JComboBoxFixture(robot(), getComboBox("", new Object[12]));
+    assertThat(cb.component().getName()).isEqualTo("");
+    cb.requireItemCount(12);
+    for (int i = 0; i < cb.component().getModel().getSize(); ++i) {
+      assertThat(cb.valueAt(i)).isEqualTo("");
+    }
+
+    cb = new JComboBoxFixture(robot(), getComboBox(null, new Object[] { "Peter", 15, "Hans" }));
+    assertThat(cb.component().getName()).isNull();
+    cb.requireItemCount(3);
+    assertThat(cb.valueAt(0)).isEqualTo("Peter");
+    assertThat(cb.valueAt(1)).isEqualTo("15");
+    assertThat(cb.valueAt(2)).isEqualTo("Hans");
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void testCstr_StringVector() {
+    JComboBoxFixture cb = new JComboBoxFixture(robot(), getComboBox("cb", new Vector<Object>()));
+    assertThat(cb.component().getName()).isEqualTo("cb");
+    cb.requireItemCount(0);
+
+    cb = new JComboBoxFixture(robot(), getComboBox("", new Vector<Object>(Arrays.asList(12, "c", null))));
+    assertThat(cb.component().getName()).isEqualTo("");
+    cb.requireItemCount(3);
+    assertThat(cb.valueAt(0)).isEqualTo("12");
+    assertThat(cb.valueAt(1)).isEqualTo("c");
+    assertThat(cb.valueAt(2)).isEqualTo("");
+
+    cb = new JComboBoxFixture(robot(), getComboBox(null, new Vector<Object>(Arrays.asList("Peter", 15, "Hans", true))));
+    assertThat(cb.component().getName()).isNull();
+    cb.requireItemCount(4);
+    assertThat(cb.valueAt(0)).isEqualTo("Peter");
+    assertThat(cb.valueAt(1)).isEqualTo("15");
+    assertThat(cb.valueAt(2)).isEqualTo("Hans");
+    assertThat(cb.valueAt(3)).isEqualTo("true");
+  }
+}
